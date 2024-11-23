c0eb2b429b1948c610a63fc587dc69f0ca699f90
TomazHaddadDev <tomazhaddad@gmail.com>
2024-11-23 20:44:13 -0300

    Files fixed

--- src/CheckPermutation.java
+++ src/CheckPermutation.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
 import java.util.HashMap;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class CheckPermutation {
+
     public static void main(String[] args) {
 
         System.out.println("POSITIVOS:");
@@ -77,6 +75,5 @@ public class Main {
         return false;
 
     }
-
 
 }
---
+++ src/URLfy.java
@@ -0,0 +1,11 @@
+public class URLfy {
+
+    public static void main(String[] args) {
+
+
+
+    }
+g
+
+
+}
